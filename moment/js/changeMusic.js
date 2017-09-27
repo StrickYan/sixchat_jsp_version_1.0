926e1b9092c74fa5eb5662890f7202b4f279e99f
归去来兮 <1275330626@qq.com>
2017-09-27 13:15:33 +0800

    Update changeMusic.js

--- moment/js/changeMusic.js
+++ moment/js/changeMusic.js
@@ -1,12 +1,12 @@
 /**
- * 
- */
-			function changeMusic(){
-				var myDate = new Date();
- 	  			// 随机换背景音乐
- 				flag = myDate.getSeconds();        //获取当前秒数(0-59)
- 				flag = flag%11;
-  				var music= document.getElementById('music');
-				music.setAttribute("src","music/"+flag+".mp3");
-			}
-			addLoadEvent(changeMusic);
+* 
+*/
+function changeMusic(){
+    var myDate = new Date();
+    // 随机换背景音乐
+    flag = myDate.getSeconds();        //获取当前秒数(0-59)
+    flag = flag%11;
+    var music= document.getElementById('music');
+    music.setAttribute("src","music/"+flag+".mp3");
+}
+addLoadEvent(changeMusic);
